e85a63608976fa2d82ac8d347bdceb0ef9b2e71f
btmnk <contact@bitmonk.de>
2023-11-12 01:49:57 +0100

    [docs] fixes base path for github pages sub path

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -5,6 +5,7 @@ import withBundleAnalyzer from "@next/bundle-analyzer";
 const nextConfig = {
   output: "export",
   pageExtensions: ["ts", "tsx", "mdx"],
+  basePath: "/rex",
 };
 
 const withMDX = createMDX({});
